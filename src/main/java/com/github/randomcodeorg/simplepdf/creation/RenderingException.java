12c9a47501d1d4f600414081c45999ce49b8d2b0
Marcel Singer <marcel.singer@live.de>
2016-04-01 13:41:11 +0200

    Documented RenderingException and RenderingInformation

--- src/main/java/com/github/randomcodeorg/simplepdf/creation/RenderingException.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/RenderingException.java
@@ -1,28 +1,45 @@
 package com.github.randomcodeorg.simplepdf.creation;
 
 
+/**
+ * This is an exception that can be thrown to indicate rendering problems.
+ * @author Marcel Singer
+ *
+ */
 public class RenderingException extends RuntimeException {
 
 	private static final long serialVersionUID = 1481910675018240284L;
 
+	/**
+	 * Creates a new instance of {@link RenderingException}.
+	 */
 	public RenderingException() {
 	}
 
+	/**
+	 * Creates a new instance of {@link RenderingException} using the given exception message.
+	 * @param message The exception message to be used.
+	 */
 	public RenderingException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Creates a new rendering exception using the given inner exception.
+	 * @param cause The causing inner exception.
+	 */
 	public RenderingException(Throwable cause) {
 		super(cause);
 	}
 
+	/**
+	 * Creates a new rendering exception using the given message and inner exception.
+	 * @param message The exception message to be used.
+	 * @param cause The causing inner exception.
+	 */
 	public RenderingException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
-	public RenderingException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
 
 }
--- src/main/java/com/github/randomcodeorg/simplepdf/creation/RenderingInformation.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/RenderingInformation.java
@@ -6,11 +6,32 @@ import com.github.randomcodeorg.simplepdf.DocumentElement;
 import com.github.randomcodeorg.simplepdf.Position;
 import com.github.randomcodeorg.simplepdf.Size;
 
+/**
+ * An interface declaring methods to request information about the current rendering process.
+ * @author Marcel Singer
+ *
+ */
 public interface RenderingInformation extends PreRenderInformation {
 
+	/**
+	 * Returns the current position.
+	 * @return The current position.
+	 */
 	Position getPosition();
+	/**
+	 * Returns the size that was reserved for the current element.
+	 * @return The size that was reserved for the current element.
+	 */
 	Size getReservedSize();
+	/**
+	 * Returns the amount of pages.
+	 * @return The amount of pages.
+	 */
 	int getPageCount();
+	/**
+	 * Returns a map containing the originated render elements for a given document element.
+	 * @return A map containing the originated render elements for a given document element.
+	 */
 	Map<DocumentElement, RenderOrigin> getOriginMap();
 	Size getParentSize();
 	
