c755677ba4e5f7f0e7b3236119bf019104d6b44f
Marcel Singer <marcel.singer@live.de>
2016-04-05 12:33:53 +0200

    Documented DocumentGraphicsCreator and ElementRenderingInformation

--- src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentGraphicsCreator.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentGraphicsCreator.java
@@ -2,16 +2,42 @@ package com.github.randomcodeorg.simplepdf.creation;
 
 import com.github.randomcodeorg.simplepdf.SimplePDFDocument;
 
+/**
+ * An interface that declares the methods that are used in order to create {@link DocumentGraphics} for a document rendering process.
+ * @author Marcel Singer
+ *
+ */
 public interface DocumentGraphicsCreator {
 
+	/**
+	 * Notifies the creator that a new document will be rendered.
+	 * @param doc The affected document.
+	 * @throws RenderingException If there was an error preparing the rendering process.
+	 */
 	public void startDocument(SimplePDFDocument doc) throws RenderingException;
 
+	/**
+	 * Returns a {@link DocumentGraphics} object that can be used to draw the next page.
+	 * @param doc The affected document.
+	 * @return A {@link DocumentGraphics} object that can be used to draw the next page.
+	 * @throws RenderingException If there was an error creating the graphics object.
+	 */
 	public DocumentGraphics nextPage(SimplePDFDocument doc)
 			throws RenderingException;
 
+	/**
+	 * Notifies the creator that the rendering process of the given document was completed.
+	 * @param doc The document thats rendering process was completed.
+	 * @throws RenderingException If there is an error during the completion.
+	 */
 	public void completeDocument(SimplePDFDocument doc)
 			throws RenderingException;
 	
+	/**
+	 * Releases all resources that are held for the given document.
+	 * @param doc The document thats resources should be released.
+	 * @throws RenderingException
+	 */
 	public void releaseDocument(SimplePDFDocument doc) throws RenderingException;
 
 }
--- src/main/java/com/github/randomcodeorg/simplepdf/creation/ElementRenderingInformation.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/ElementRenderingInformation.java
@@ -4,21 +4,52 @@ import com.github.randomcodeorg.simplepdf.DocumentElement;
 import com.github.randomcodeorg.simplepdf.Position;
 import com.github.randomcodeorg.simplepdf.Size;
 
+/**
+ * A class that can be used to encapsulate information about the render process of a given render element.
+ * @author Marcel Singer
+ *
+ */
 public class ElementRenderingInformation {
 
+	/**
+	 * Hold the elements render position.
+	 */
 	private final Position location;
+	/**
+	 * Holds the elements render size.
+	 */
 	private final Size size;
+	/**
+	 * Holds the render element.
+	 */
 	private final RenderElement<? extends DocumentElement> element;
 	
-	
+	/**
+	 * Creates a new instance of {@link ElementRenderingInformation} using the given information.
+	 * @param element The element the information is about.
+	 * @param location The given elements render position.
+	 * @param size The given elements render size.
+	 */
 	public ElementRenderingInformation(RenderElement<? extends DocumentElement> element, Position location, Size size) {
 		this.location = location;
 		this.size = size;
 		this.element = element;
 	}
 	
+	/**
+	 * Returns the elements render position.
+	 * @return The elements render position.
+	 */
 	public Position getLocation(){ return location; }
+	/**
+	 * Returns the elements render size.
+	 * @return The elements render size.
+	 */
 	public Size getSize(){ return size; }
+	/**
+	 * Returns the element this rendering information is about.
+	 * @return The element this rendering information is about.
+	 */
 	public RenderElement<? extends DocumentElement> getElement(){ return element; }
 
 	
