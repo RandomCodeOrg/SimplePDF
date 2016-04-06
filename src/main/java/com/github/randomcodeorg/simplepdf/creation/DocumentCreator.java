48753c42f8ba97ac63314889cf882e5eceab32b2
Marcel Singer <marcel.singer@live.de>
2016-04-06 12:04:10 +0200

    Documented DocumentGraphics and DocumentCreator

--- src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentCreator.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentCreator.java
@@ -9,16 +9,42 @@ import com.github.randomcodeorg.simplepdf.AreaDefinition;
 import com.github.randomcodeorg.simplepdf.DocumentElement;
 import com.github.randomcodeorg.simplepdf.SimplePDFDocument;
 
+/**
+ * <p>A {@link DocumentCreator} is used to render a given {@link SimplePDFDocument} and export the result in a certain format.
+ * One can use this class directly by instantiating it with the desired implementation of {@link DocumentGraphicsCreator}.</p>
+ * <p><b>Note:</b> There following specialized {@link DocumentCreator} implementations are available for your comfort:</p> 
+ *  <ul>
+ *  <li>To render to the PDF format: {@link PDDocumentCreator} (uses {@link PDDocumentGraphicsCreator})</li>
+ *  <li>To render to an image format:  {@link ImageDocumentCreator} (uses {@link ImageDocumentGraphicsCreator})</li>
+ *  </ul>
+ * @author Marcel Singer
+ *
+ */
 public class DocumentCreator implements ConversionConstants {
 
+	/**
+	 * Holds the render mapping to be used.
+	 */
 	private ElementRenderMapping renderMapping = ElementRenderMapping.getDefault();
 
+	/**
+	 * Holds the document graphics creator to be used.
+	 */
 	private final DocumentGraphicsCreator documentGraphicsCreator;
 
+	/**
+	 * Creates a new instance of {@link DocumentCreator} that uses the given {@link DocumentGraphicsCreator}.
+	 * @param documentGraphicsCreator The document graphics creator to be used.
+	 */
 	public DocumentCreator(DocumentGraphicsCreator documentGraphicsCreator) {
 		this.documentGraphicsCreator = documentGraphicsCreator;
 	}
 
+	/**
+	 * Renders the given document.
+	 * @param doc The document to be rendered.
+	 * @throws RenderingException Is thrown if there is an error during the rendering.
+	 */
 	public void create(SimplePDFDocument doc) throws RenderingException {
 		documentGraphicsCreator.startDocument(doc);
 		List<DocumentArea> areas = new ArrayList<DocumentArea>();
@@ -35,12 +61,24 @@ public class DocumentCreator implements ConversionConstants {
 		documentGraphicsCreator.completeDocument(doc);
 	}
 
+	/**
+	 * Disposes all layouts contained in the given {@link Iterable}.
+	 * @param layouts The layouts to be disposed.
+	 */
 	private void cleanup(Iterable<AreaLayout> layouts) {
 		for (AreaLayout al : layouts) {
 			al.dispose();
 		}
 	}
 
+	/**
+	 * Draws the elements after the layout phase completed.
+	 * @param doc The document that is currently rendered.
+	 * @param layouts The result of the the layout phase.
+	 * @param areas The resulting document areas.
+	 * @param originMap The origin map.
+	 * @throws RenderingException Is thrown if there is an error during the rendering.
+	 */
 	private void render(SimplePDFDocument doc, Iterable<AreaLayout> layouts, Iterable<DocumentArea> areas, Map<DocumentElement, RenderOrigin> originMap) throws RenderingException {
 		int pageLength = 0;
 		for (AreaLayout al : layouts)
@@ -56,6 +94,14 @@ public class DocumentCreator implements ConversionConstants {
 		}
 	}
 
+	/**
+	 * Performs the layout of the contained document elements.
+	 * @param layouts The area layouts.
+	 * @param doc The currently created document.
+	 * @param pageIndex The current page index.
+	 * @param areas The document areas.
+	 * @throws RenderingException Is thrown if thers is an error during the layout process.
+	 */
 	private void layout(List<AreaLayout> layouts, SimplePDFDocument doc, int pageIndex, List<DocumentArea> areas)
 			throws RenderingException {
 		if (areas.size() == 0)
@@ -85,6 +131,10 @@ public class DocumentCreator implements ConversionConstants {
 		layout(layouts, doc, pageIndex + 1, following);
 	}
 
+	/**
+	 * Returns the document graphics creator used by this instance.
+	 * @return The document graphics creator used by this instance.
+	 */
 	protected final DocumentGraphicsCreator getDocumentGraphicsCreator() {
 		return documentGraphicsCreator;
 	}
--- src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentGraphics.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentGraphics.java
@@ -6,18 +6,68 @@ import com.github.randomcodeorg.simplepdf.Position;
 import com.github.randomcodeorg.simplepdf.Size;
 import com.github.randomcodeorg.simplepdf.StyleDefinition;
 
+/**
+ * An interface declaring methods to be used to draw and render a document.
+ * @author Marcel Singer
+ *
+ */
 public interface DocumentGraphics {
 
+	/**
+	 * Renders the given text at the given position. 
+	 * @param text The text to render.
+	 * @param p The position.
+	 * @param sd The style definition to be applied.
+	 * @param reservedSize The size that was reserved for the given text.
+	 * @param isSingleLine <code>true</code> if the given text is a single line.
+	 * @throws RenderingException This exception is thrown if there is an error during the rendering.
+	 */
 	public void drawText(String text, Position p, StyleDefinition sd, Size reservedSize, boolean isSingleLine) throws RenderingException;
 
+	/**
+	 * Renders a line.
+	 * @param start The start point of the line to render.
+	 * @param end The end point of the line to render.
+	 * @param lineWidth The line width to be used.
+	 * @param sd The style definition to be applied.
+	 * @throws RenderingException This exception is thrown if there is an error during the rendering.
+	 */
 	public void drawLine(Position start, Position end, double lineWidth, StyleDefinition sd) throws RenderingException;
 
+	/**
+	 * Renders an image at the given position.
+	 * @param p The position.
+	 * @param s The expected size of the image.
+	 * @param image The image to render.
+	 * @param sd The style definition to be applied.
+	 * @throws RenderingException This exception is thrown if there is an error during the rendering.
+	 */
 	public void drawImage(Position p, Size s, BufferedImage image, StyleDefinition sd) throws RenderingException;
 
+	/**
+	 * Measures the size of the given text.
+	 * @param text The text to measure.
+	 * @param sd The style definition to be applied.
+	 * @param reservedSize The reserved size.
+	 * @return The measured size of the given text.
+	 * @throws RenderingException This exception is thrown if there is an error during the measuring.
+	 */
 	public Size getTextSize(String text, StyleDefinition sd, Size reservedSize) throws RenderingException;
 	
+	/**
+	 * Draws an rectangle at the given position.
+	 * @param p The position.
+	 * @param s The size of the rectangle.
+	 * @param lineWidth The line width to be used.
+	 * @param sd The style definition to be applied.
+	 * @throws RenderingException This exception is thrown if there is an error during the rendering.
+	 */
 	public void drawRect(Position p, Size s, double lineWidth, StyleDefinition sd) throws RenderingException;
 
+	/**
+	 * Releases all resources held by this instance.
+	 * @throws RenderingException This exception is thrown if there is an error during the release of held resources.
+	 */
 	public void dispose() throws RenderingException;
 
 }
