82a5ca0dee4d1d8f42ed1e9632bf0980360a04cb
Marcel Singer <marcel.singer@live.de>
2016-04-07 14:14:18 +0200

    Documented CombinedProcessListener and ConversionConstants

--- src/main/java/com/github/randomcodeorg/simplepdf/creation/CombinedProcessListener.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/CombinedProcessListener.java
@@ -4,14 +4,30 @@ import java.util.Arrays;
 
 import com.github.randomcodeorg.simplepdf.ProcessMessage;
 
+/**
+ * An implementation of {@link ProcessListener} that can be used to redirect incoming messages to multiple other listeners.
+ * @author Marcel Singer
+ *
+ */
 public class CombinedProcessListener implements ProcessListener {
 
+	/**
+	 * A field holding the listeners to redirect to.
+	 */
 	private final Iterable<ProcessListener> listeners;
 
+	/**
+	 * Creates a new instance of {@link CombinedProcessListener} using the given listeners.
+	 * @param listeners The target listeners to redirect to.
+	 */
 	public CombinedProcessListener(Iterable<ProcessListener> listeners) {
 		this.listeners = listeners;
 	}
 
+	/**
+	 * Creates a new instance of {@link CombinedProcessListener} using the given listeners.
+	 * @param listeners The target listeners to redirect to.
+	 */
 	public CombinedProcessListener(ProcessListener... listeners) {
 		this(Arrays.asList(listeners));
 	}
--- src/main/java/com/github/randomcodeorg/simplepdf/creation/ConversionConstants.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/ConversionConstants.java
@@ -1,8 +1,19 @@
 package com.github.randomcodeorg.simplepdf.creation;
 
+/**
+ * An interface declaring constant that can be used to convert between different types of units.
+ * @author Marcel Singer
+ *
+ */
 public interface ConversionConstants {
 	
+	/**
+	 * A constant that can be used to convert millimeters to units.
+	 */
 	public static final float MM_TO_UNITS = (72.0f / 25.4f);
+	/**
+	 * A constant that can be used to convert units to millimeters.
+	 */
 	public static final float UNITS_TO_MM = (25.4f / 72.0f);
 
 }
