0274d230f868d3b94b70914159db44b26e92a975
Marcel Singer <marcel.singer@live.de>
2016-04-07 14:11:00 +0200

    Documented DefaultProcessListener and DocumentArea

--- src/main/java/com/github/randomcodeorg/simplepdf/creation/DefaultProcessListener.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/DefaultProcessListener.java
@@ -5,10 +5,21 @@ import com.github.randomcodeorg.simplepdf.ProcessMessage;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is the default implementation of {@link ProcessListener}. It will store all incoming events.
+ * @author Marcel Singer
+ *
+ */
 public class DefaultProcessListener implements ProcessListener {
 
+	/**
+	 * A list that is used to store incoming messages.
+	 */
 	private final List<ProcessMessage> messages = new ArrayList<ProcessMessage>();
 
+	/**
+	 * Creates a new instance of {@link DefaultProcessListener}.
+	 */
 	public DefaultProcessListener() {
 			
 	}
@@ -28,7 +39,16 @@ public class DefaultProcessListener implements ProcessListener {
 
 	}
 	
+	/**
+	 * Returns the amount of currently stored messages.
+	 * @return The amount of currently stored messages.
+	 */
 	public int getMessageCount(){ return messages.size(); }
+	/**
+	 * Returns the message at the given index.
+	 * @param index The index of the message to be returned..
+	 * @return The message at the given index.
+	 */
 	public ProcessMessage getMessage(int index){ return messages.get(index); }
 
 }
--- src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentArea.java
+++ src/main/java/com/github/randomcodeorg/simplepdf/creation/DocumentArea.java
@@ -14,18 +14,54 @@ import com.github.randomcodeorg.simplepdf.SimplePDFDocument;
 import com.github.randomcodeorg.simplepdf.Size;
 import com.github.randomcodeorg.simplepdf.Spacing;
 
+/**
+ * A {@link DocumentArea} is used to support the layout process of a given {@link AreaDefinition}.
+ * @author Marcel Singer
+ *
+ */
 public class DocumentArea {
 
+	/**
+	 * The {@link AreaDefinition} affected by this instance.
+	 */
 	private final AreaDefinition area;
+	/**
+	 * The currently created document.
+	 */
 	private final SimplePDFDocument document;
+	/**
+	 * The element render mapping to be used.
+	 */
 	private final ElementRenderMapping renderMapping;
+	/**
+	 * A list of elements that where skipped because they could not be positioned within this area.
+	 */
 	private final List<RenderElement<? extends DocumentElement>> skippedElements;
+	/**
+	 * A list of elements that where successfully positioned within this area. 
+	 */
 	private final List<RenderElement<? extends DocumentElement>> elements;
 
+	/**
+	 * The current horizontal position.
+	 */
 	private float x = 0;
+	/**
+	 * The current vertical position.
+	 */
 	private float y = 0;
+	/**
+	 * The currently reserved horizontal spacing.
+	 */
 	private float dY = 0;
 
+	/**
+	 * Creates a new instance of {@link DocumentArea}.
+	 * @param renderMapping The render mapping to be used.
+	 * @param ad The affected area definition.
+	 * @param doc The containing document.
+	 * @param renderOriging A map storing the originating {@link DocumentElement}s.
+	 */
 	public DocumentArea(ElementRenderMapping renderMapping, AreaDefinition ad, SimplePDFDocument doc,
 			Map<DocumentElement, RenderOrigin> renderOriging) {
 		this.skippedElements = new LinkedList<RenderElement<? extends DocumentElement>>();
@@ -47,6 +83,14 @@ public class DocumentArea {
 		}
 	}
 
+	/**
+	 * Creates a new instance of {@link DocumentArea}.
+	 * @param renderMapping The render mapping to be used.
+	 * @param ad The affected area definition.
+	 * @param doc The containing document.
+	 * @param elements The elements that were already positioned.
+	 * @param skippedElements The elements that were skipped.
+	 */
 	private DocumentArea(ElementRenderMapping renderMapping, AreaDefinition ad, SimplePDFDocument doc,
 			List<RenderElement<? extends DocumentElement>> elements,
 			List<RenderElement<? extends DocumentElement>> skippedElements) {
@@ -57,6 +101,10 @@ public class DocumentArea {
 		this.skippedElements = skippedElements;
 	}
 
+	/**
+	 * Returns the following (on the next page) document area.
+	 * @return The following document area or <code>null</code> if affected area definition has a limited availability.
+	 */
 	public DocumentArea next() {
 		if (area.getAvailability() == AreaAvailability.ONLY_FIRST_PAGE)
 			return null;
@@ -64,6 +112,10 @@ public class DocumentArea {
 		return docArea;
 	}
 
+	/**
+	 * Returns <code>true</code> if all remaining elements are repeating.
+	 * @return <code>true</code> if all remaining elements are repeating.
+	 */
 	public boolean onlyRepeats() {
 		for (RenderElement<? extends DocumentElement> re : elements) {
 			if (!re.isRepeating())
@@ -72,14 +124,27 @@ public class DocumentArea {
 		return true;
 	}
 
+	/**
+	 * Returns the current translated x position.
+	 * @return The current translated x position.
+	 */
 	private float getTranslatedX() {
 		return x + area.getPosition().getX();
 	}
 
+	/**
+	 * Returns the current translated y position.
+	 * @return The current translated y position.
+	 */
 	private float getTranslatedY() {
 		return y + area.getPosition().getY();
 	}
 
+	/**
+	 * Moves the current position by the given size.
+	 * @param element The element associated with the given size.
+	 * @param size The size to be used.
+	 */
 	private void move(RenderElement<? extends DocumentElement> element, Size size) {
 		if (element.isLineBreak()) {
 			x = 0;
@@ -93,6 +158,13 @@ public class DocumentArea {
 
 	}
 
+	/**
+	 * Returns <code>true</code> if the given element fits inside this document area.
+	 * @param docElement The document element.
+	 * @param info Information about the current document creation process.
+	 * @return <code>true</code> if the given element fits inside this document area.
+	 * @throws RenderingException Is thrown if there is an error during this check.
+	 */
 	public boolean canHold(RenderElement<? extends DocumentElement> docElement, PreRenderInformation info)
 			throws RenderingException {
 		Size totalElementSize = docElement.getTotalSize(info, area.getSize());
@@ -103,12 +175,26 @@ public class DocumentArea {
 		return true;
 	}
 
+	/**
+	 * Returns the remaining available size.
+	 * @param elementMargin The margin to assume.
+	 * @return The remaining available size.
+	 */
 	private Size getAvailableSize(Spacing elementMargin) {
 		float xS = (float) (area.getSize().getWidth() - x - elementMargin.getLeft() - elementMargin.getRight());
 		float yS = (float) (area.getSize().getHeight() - y - elementMargin.getBottom() - elementMargin.getTop());
 		return new Size(xS, yS);
 	}
 
+	/**
+	 * Splits the given element.
+	 * @param element The element to be split.
+	 * @param g The document graphics to be used.
+	 * @param layout The current area layout.
+	 * @param areas All currently available document areas.
+	 * @return A list containing the split result.
+	 * @throws RenderingException Is thrown if there is an error during the split process.
+	 */
 	private List<RenderElement<? extends DocumentElement>> doSplit(RenderElement<? extends DocumentElement> element,
 			DocumentGraphics g, AreaLayout layout, Iterable<DocumentArea> areas) throws RenderingException {
 		Spacing spacing = element.getRenderMargin(g);
@@ -116,6 +202,11 @@ public class DocumentArea {
 		return element.splitToFit(new PreRenderInformationImpl(document, areas, layout, g, ElementRenderMapping.getDefault()), s);
 	}
 
+	/**
+	 * Performs the layout process.
+	 * @param info Information about the current document creation process.
+	 * @throws RenderingException Is thrown if there is an error during the layout phase.
+	 */
 	public void layout(PreRenderInformation info) throws RenderingException {
 		if (elements.size() == 0)
 			return;
